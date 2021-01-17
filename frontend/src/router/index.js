8ddc26e9870d4e5b6eed6360e85dfdb10afbcb96
Steve51100 <sttaz51@hotmail.fr>
2021-01-17 17:47:51 +0100

    ajout des fichers projet

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,21 +1,57 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Auth from '../components/Auth.vue';
+import Accueil from '../components/Acceuil.vue';
+import Profil from '../components/Accueil/Profil.vue';
+import Mur from '../components/Accueil/Mur.vue';
+import Post from '../components/Accueil/FormPost.vue';
+import Gif from '../components/Accueil/FormGifs.vue';
+import Moderation from '../components/Accueil/Moderation.vue';
 
 Vue.use(VueRouter)
 
-const routes =[
-    {
-        path: '/',
-        name: 'Auth',
-        component: Auth
-      },
+const routes = [
+  {
+    path: '/',
+    name: 'Auth',
+    component: Auth
+  },
+  {
+    path: '/Accueil',
+    name: 'Accueil',
+    component: Accueil
+  },
+ 
+  {
+    path: '/Accueil/Profil',
+    name: 'Profil',
+    component: Profil
+  },
+  {
+    path: '/Accueil/Mur',
+    name: 'Mur',
+    component: Mur
+  },
+  {
+    path:'/Accueil/Gifs',
+    name: 'Gif',
+    component: Gif
+  },
+  {
+    path: '/Accueil/Mur/Post',
+    name: 'Post',
+    component: Post
+  },
+  {
+    path: '/Accueil/Moderation',
+    name: 'Moderation',
+    component: Moderation
+  },
 ]
 
-
 const router = new VueRouter({
-    routes,
-    mode: 'history'
-  })
+  routes,
+  mode: 'history'
+})
   
   export default router
